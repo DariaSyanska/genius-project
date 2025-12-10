af21ad0fda2d0384f81146c93c2c963cd2b08a93
MacBook <macbook@MacBook-Pro-MacBook.local>
2025-12-10 14:56:49 +0100

    Update the project

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,7 +1,38 @@
+const sliderList = document.querySelector('.formats-section__list');
+
+if (sliderList) {
+  const slides = sliderList.querySelectorAll('.swiper-slide');
+
+  if (slides[1]) {
+    slides[1].querySelector('.format-card__text').innerHTML = `
+      <p>Площа: до 25 м2</p>
+      <p>Меню: найпопулярніші позиції меню</p>
+      <p>Штат: 2 продавці-касири, 2 пекарі, 1 помічник пекаря</p>
+      <p>Посадкові місця: відсутні</p>
+    `;
+  }
+
+  if (slides[2]) {
+    slides[2].querySelector('.format-card__text').innerHTML = `
+      <p>Площа: до 15 м2</p>
+      <p>Меню: найпопулярніші позиції меню</p>
+      <p>Штат: 1 продавець, 1 пекар, 1 помічник пекаря</p>
+      <p>Посадкові місця: відсутні</p>
+    `;
+  }
+
+  for (let i = 4; i < slides.length; i++) {
+    slides[i].remove();
+  }
+}
+
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
+
+  loopedSlides: 4,
+
   breakpoints: {
     768: {
       slidesPerView: 2,
